fbe22e27e3506cd93742f88f75f7e314db23f52e
Roberto Alicata <roberto.alicata@gmail.com>
2017-05-11 12:34:46 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const mainRouter = require('./routes/index');
+const apiRouter = require('./routes/api');
+
+const PORT = process.env.PORT || 8080;
+const DB = "mongodb://localhost/angularHuman";
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', mainRouter);
+app.use('/api', apiRouter);
+
+mongoose.connect(DB, (err) => {
+    if(err){
+        console.log(err);
+        return err;
+    } else {
+        console.log("Successfully connected to " + DB);
+    }
+});
+
+// SET view engine
+app.set('views', './client');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// SET static 
+app.use(express.static(__dirname + '/client'));
+
+app.listen(PORT, () => {
+    console.log('listening on port '+ PORT);
+});
